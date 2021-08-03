4b8e1dcc45e6eed21116d43ee72a31c784b2895a
ParkByeolYi <51067412+ParkByeolYi@users.noreply.github.com>
2021-08-03 09:50:58 +0900

    Update 1051.py

--- Algorithm/BYEOLYI/1051.py
+++ Algorithm/BYEOLYI/1051.py
@@ -10,14 +10,14 @@ for _ in range(n):
 
 for i in range(n):
   for j in range(m):
-    for k in range(n):
-      if i + k < n and j + k < m:
+    for k in range(n): # 정사각형 찾기
+      if i + k < n and j + k < m: # 데이터에서 범위 벗어나지 않을 경우
         left1 = data[i][j]
         left2 = data[i][j + k]
         right1 = data[i + k][j]
         right2 = data[i + k][j + k]
 
-        if left1 == left2 and left2 == right1 and right1 == right2:
-          result = max(result, (k + 1) * (k + 1))
+        if left1 == left2 and left2 == right1 and right1 == right2: # 꼭짓점에 쓰여 있는 수가 모두 같을 경우
+          result = max(result, (k + 1) * (k + 1)) # 이전 값과 비교했을 때 큰 값을 저장
 
 print(result)
