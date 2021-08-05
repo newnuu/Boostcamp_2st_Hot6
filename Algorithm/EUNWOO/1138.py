d83be4700c4e93f4f4a1cc6efbf7f988a397a5a2
EUNWOO LEE <68593821+newnuu@users.noreply.github.com>
2021-08-05 23:32:44 +0900

    Update 1138.py

--- Algorithm/EUNWOO/1138.py
+++ Algorithm/EUNWOO/1138.py
@@ -2,7 +2,8 @@ n = int(input())
 line = list(map(int,input().split()))
 answer = [len(line)]
 
+# 뒤에서부터 
 for i in range(len(line)-2,-1,-1):
-    answer = answer[:line[i]]+[i+1]+answer[line[i]:]
+    answer = answer[:line[i]]+[i+1]+answer[line[i]:] # 앞에 큰 사람이 있는 수대로 앞에서부터 잘라서 중간에 넣기
 for i in answer:
     print(i,end=' ')
