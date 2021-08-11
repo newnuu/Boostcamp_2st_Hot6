2a19fcc2b4c97fb1726fc629bd81501afa11e90b
EUNWOO LEE <68593821+newnuu@users.noreply.github.com>
2021-08-11 11:02:55 +0900

    Update 5710.py

--- Algorithm/EUNWOO/5710.py
+++ Algorithm/EUNWOO/5710.py
@@ -1,4 +1,4 @@
-def fare(x):
+def fare(x): # 요금 구하는 함수
     if x>1000000:
         return 7*(x-1000000)+5*990000+3*9900+2*100
     elif x>10000:
@@ -8,7 +8,7 @@ def fare(x):
     else:
         return 2*x
 
-def elec(fare):
+def elec(fare): # 요금으로 전기 사용량 구하는 함수
     if fare>(2*100+3*9900+5*990000):
         return 1000000+(fare-(2*100+3*9900+5*990000))//7
     elif fare > (2*100+3*9900):
@@ -22,11 +22,13 @@ while True:
     s,d = map(int,input().split())
     if s==0 and d==0:
         break
-    s1 = elec(s)
+    s1 = elec(s) # 사용량 합
     b = s1//2
     a = s1-b
     dd = abs(fare(b)-fare(a))
     dif = s1-b
+    
+    # 이분탐색 두 전기요금의 차(dd)가 d와 같을 때까지
     while dd!=d:
         dif = dif//2 if dif//2!=0 else 1
         if dd<d:
@@ -35,4 +37,4 @@ while True:
             b-=dif
         a = s1-b
         dd = abs(fare(b)-fare(a))
-    print(min(fare(a),fare(b)))
+    print(min(fare(a),fare(b))) # 두 요금 중 더 작은 값
