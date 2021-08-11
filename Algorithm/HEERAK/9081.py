ebcb1c7dbee081a1236d50144111dcd6661a26dd
raki-1203 <jfhdzzang@gmail.com>
2021-08-12 00:19:49 +0900

    .

--- Algorithm/HEERAK/9081.py
+++ Algorithm/HEERAK/9081.py
@@ -2,12 +2,18 @@ from sys import stdin
 input = stdin.readline
 
 def solution(char_list):
+    # 문자열 뒤에서부터 비교
     for i in range(len(char_list) - 1, 0, -1):
+        # 만약 뒤의 문자열이 앞의 문자열보다 크다면(사전순으로 뒤에 나온다면)
         if char_list[i - 1] < char_list[i]:
+            # 기준인 i는 그 앞문자의 index
             i -= 1
             for j in range(len(char_list) - 1, 0, -1):
+                # 기준인 i번째 문자보다 큰 j번째 문자를 찾고
                 if char_list[i] < char_list[j]:
+                    # i번쨰 문자와 j번째 문자를 바꾸면
                     char_list[i], char_list[j] = char_list[j], char_list[i]
+                    # i번째 문자까지는 모두 그대로고 i+1번쨰 문자열 부터는 사전순으로 정렬해서 concatenate
                     return char_list[:i + 1] + sorted(char_list[i + 1:])
     return char_list
 
