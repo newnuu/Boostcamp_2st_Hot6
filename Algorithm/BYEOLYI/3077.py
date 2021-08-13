09b7ce89b3dac0c6f52e8d8a555c18fd140dfea4
ParkByeolYi <51067412+ParkByeolYi@users.noreply.github.com>
2021-08-13 11:06:18 +0900

    Create 3077.py

---
+++ Algorithm/BYEOLYI/3077.py
@@ -0,0 +1,21 @@
+# 3077번  임진왜란  실버 3
+
+from sys import stdin
+
+n = int(stdin.readline())
+correct_answer = list(stdin.readline().split())
+student_answer = list(stdin.readline().split())
+# 학생 점수, 총점
+a, b = 0, n * (n - 1) // 2
+
+for i in range(n):
+  for j in range(i + 1, n):
+    student1 = student_answer.index(correct_answer[i])
+    student2 = student_answer.index(correct_answer[j])
+
+    print(student1, student2)
+    
+    if student1 < student2:
+      a += 1
+      
+print(str(a) + "/" + str(b))
