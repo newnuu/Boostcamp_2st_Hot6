e8f1f6a5588ad0c6df61c01397420d792e3e7c41
이상준_T2158 <85214907+sangjun-Leee@users.noreply.github.com>
2021-08-14 13:32:20 +0900

    Update 3182.py

--- Algorithm/Sangjun/3182.py
+++ Algorithm/Sangjun/3182.py
@@ -1,24 +1,29 @@
 from sys import stdin
+input = stdin.readline
 
-n = int(stdin.readline())
-reply = [int(stdin.readline()) for _ in range(n)]
-meet = [0] * n
-meet_num = []
+def _3182(n:int, lt:list):
 
-# 모든 선배로부터 시작해 본다.
-for idx, val in enumerate(reply):
+    meet_num = []
 
-    # 다음 선배로 갈 때 이미 만난 선배인지 확인하는 list
-    meet = [0] * n
-    
-    # 만나지 않은 선배라면 만났다는 표시를 해준 후 다음 선배로 이동 
-    while meet[idx] == 0:
-        meet[idx] = 1
-        idx = val - 1
-        val = reply[idx]
-    
-    # 만난 선배의 수 저장
-    meet_num.append(sum(meet))
+    # 모든 선배로부터 시작해 본다.
+    for idx, val in enumerate(lt):
 
-print(meet_num.index(max(meet_num)) + 1)
+        # 다음 선배로 갈 때 이미 만난 선배인지 확인하는 list
+        meet = [0] * n
 
+        # 만나지 않은 선배라면 만났다는 표시를 해준 후 다음 선배로 이동 
+        while meet[idx] == 0:
+            meet[idx] = 1
+            idx = val - 1
+            val = lt[idx]
+
+        # 만난 선배의 수 저장
+        meet_num.append(sum(meet))
+
+    print(meet_num.index(max(meet_num)) + 1)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    reply = [int(input()) for _ in range(n)]
+    _3182(n, reply)
