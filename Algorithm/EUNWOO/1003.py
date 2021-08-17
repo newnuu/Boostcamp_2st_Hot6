b945f7117933a8502f1fcc55fcab280d0e2d6df8
EUNWOO LEE <68593821+newnuu@users.noreply.github.com>
2021-08-17 23:59:27 +0900

    Create 1003.py

---
+++ Algorithm/EUNWOO/1003.py
@@ -0,0 +1,7 @@
+t = int(input())
+fib = [(1,0),(0,1)]
+for i in range(2,41):
+    fib.append((fib[i-1][0]+fib[i-2][0],fib[i-1][1]+fib[i-2][1]))
+for _ in range(t):
+    n = int(input())
+    print(*fib[n])
