be6143a486ee76cc826ae6cb3470dbaceb78e616
EUNWOO LEE <68593821+newnuu@users.noreply.github.com>
2021-08-18 00:05:40 +0900

    Update 1003.py

--- Algorithm/EUNWOO/1003.py
+++ Algorithm/EUNWOO/1003.py
@@ -1,5 +1,5 @@
 t = int(input())
-fib = [(1,0),(0,1)]
+fib = [(1,0),(0,1)] # (0의 호출수, 1의 호출수)
 for i in range(2,41):
     fib.append((fib[i-1][0]+fib[i-2][0],fib[i-1][1]+fib[i-2][1]))
 for _ in range(t):
