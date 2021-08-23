7647bdf59dc65c76aba121afffe7c4a74aa7aecf
이상준_T2158 <85214907+sangjun-Leee@users.noreply.github.com>
2021-08-23 16:07:18 +0900

    Update 2636.py

--- Algorithm/Sangjun/2636.py
+++ Algorithm/Sangjun/2636.py
@@ -2,6 +2,8 @@ from sys import stdin
 from collections import deque
 input = stdin.readline
 
+# 구멍이라면 탐색했을 때 치즈틀에 도달 할 수 없음 <- 이걸 생각 못하고 치즈 가장자리만 구분해 내려고 개고생
+
 def _2636(R : int, C : int, borad : list):
     visited = [[0] * C for _ in range(R)]
     dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
@@ -9,7 +11,7 @@ def _2636(R : int, C : int, borad : list):
     queue.append((0, 0))
     visited[0][0] = 1
     cnt = 0
-
+    
     while queue:
         x, y = queue.popleft()
         for i in range(4):
