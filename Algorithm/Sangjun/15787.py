3db3b5f17985478961cb036c9133cfcfd079dd3d
이상준_T2158 <85214907+sangjun-Leee@users.noreply.github.com>
2021-08-24 11:04:42 +0900

    Create 15787.py

---
+++ Algorithm/Sangjun/15787.py
@@ -0,0 +1,40 @@
+from sys import stdin
+input = stdin.readline
+
+
+def _15787(N : int, command : list):
+    train = [[0] * 20 for _ in range(N)]
+    for cmd in command:
+
+        # 1번 명령 : 사람 없을 때만 고려
+        if cmd[0] == 1:
+            if not train[cmd[1] - 1][cmd[2] - 1]:
+                train[cmd[1] - 1][cmd[2] - 1] = True
+        
+        # 2번 명령 : 사람 있을 때만 고려
+        elif cmd[0] == 2:
+            if train[cmd[1] - 1][cmd[2] - 1]:
+                train[cmd[1] - 1][cmd[2] - 1] = 
+        
+        # 3번 명령 : 마지막 사람 자르고 앞에 안탔다는 표시
+        elif cmd[0] == 3:
+            train[cmd[1] - 1] = [0] + train[cmd[1] - 1][:-1]
+        
+        # 4번 명령 : 앞사람 자르고 마지막에 안탔다는 표시
+        elif cmd[0] == 4:
+            train[cmd[1] - 1] = train[cmd[1] - 1][1:] + [0]
+    
+    
+    # 배열같은 기차 제거
+    pass_train = []
+    for i in train:
+        if i not in pass_train:
+            pass_train.append(i)
+
+    print(len(pass_train))
+
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    command = [list(map(int, input().split())) for _ in range(M)]
+    _15787(N, command)
