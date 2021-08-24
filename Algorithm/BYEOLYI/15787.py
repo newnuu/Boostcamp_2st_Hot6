4e167df7b1b4a5d25d69d12e18c3bd9752784a45
ParkByeolYi <51067412+ParkByeolYi@users.noreply.github.com>
2021-08-24 13:03:09 +0900

    Update 15787.py

--- Algorithm/BYEOLYI/15787.py
+++ Algorithm/BYEOLYI/15787.py
@@ -11,18 +11,18 @@ for _ in range(m):
     data = list(map(int,input().split()))
 
     if data[0] == 1:
-        train[data[1]-1][data[2]-1]=1
+        train[data[1]-1][data[2]-1] = 1
 
     elif data[0] == 2:
-        train[data[1]-1][data[2]-1]=0
+        train[data[1]-1][data[2]-1] = 0
 
     elif data[0] == 3:
         train[data[1]-1].rotate(1)
-        train[data[1]-1][0]=0
+        train[data[1]-1][0] = 0
 
     elif data[0] == 4:
         train[data[1]-1].rotate(-1)
-        train[data[1]-1][19]=0
+        train[data[1]-1][19] = 0
 
 result = []
 
