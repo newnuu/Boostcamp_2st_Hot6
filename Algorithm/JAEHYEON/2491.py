7f461ea9ecf1cd2941b267eb24a70ebb747a8242
jaehyeon-git <64246382+jaehyeon-git@users.noreply.github.com>
2021-08-30 20:46:27 +0900

    Create 2491.py

---
+++ Algorithm/JAEHYEON/2491.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input()) # 수열의 길이
+seq_list = list(map(int, input().split())) # 순열 리스트
+
+cnt = 1
+max_length = 1
+
+#점점 작아지는 순열 찾기
+for i in range(1, N):
+    if seq_list[i - 1] >= seq_list[i]:
+        cnt += 1
+    else:
+        cnt = 1
+    if max_length <= cnt:
+        max_length = cnt
+
+cnt = 1
+# 점점 커지는 순열 찾기
+for i in range(1, N):
+    if seq_list[i - 1] <= seq_list[i]:
+        cnt += 1
+    else:
+        cnt = 1
+    if max_length < cnt:
+        max_length = cnt
+
+print(max_length)
